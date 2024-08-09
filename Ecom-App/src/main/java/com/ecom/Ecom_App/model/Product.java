fc83a3b9b69f021bde4d7b71689fa1894f9ad23f
sweetyswanthika <146558428+sweetyswanthika@users.noreply.github.com>
2024-08-09 09:11:20 -0700

    Add files via upload

---
+++ Ecom-App/src/main/java/com/ecom/Ecom_App/model/Product.java
@@ -0,0 +1,155 @@
+package com.ecom.Ecom_App.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue
+    (strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String description;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+
+    private Date releaseDate;
+    private boolean productAvailable;
+    private int stockQuantity;
+
+    private String imageName;
+    private String imageType;
+    
+    @Lob
+    private byte[] imageDate;
+    
+	public Product() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Product(int id, String name, String description, String brand, BigDecimal price, String category,
+			Date releaseDate, boolean productAvailable, int stockQuantity, String imageName, String imageType,
+			byte[] imageDate) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.brand = brand;
+		this.price = price;
+		this.category = category;
+		this.releaseDate = releaseDate;
+		this.productAvailable = productAvailable;
+		this.stockQuantity = stockQuantity;
+		this.imageName = imageName;
+		this.imageType = imageType;
+		this.imageDate = imageDate;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public Date getReleaseDate() {
+		return releaseDate;
+	}
+
+	public void setReleaseDate(Date releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+
+	public boolean isProductAvailable() {
+		return productAvailable;
+	}
+
+	public void setProductAvailable(boolean productAvailable) {
+		this.productAvailable = productAvailable;
+	}
+
+	public int getStockQuantity() {
+		return stockQuantity;
+	}
+
+	public void setStockQuantity(int stockQuantity) {
+		this.stockQuantity = stockQuantity;
+	}
+
+	public String getImageName() {
+		return imageName;
+	}
+
+	public void setImageName(String imageName) {
+		this.imageName = imageName;
+	}
+
+	public String getImageType() {
+		return imageType;
+	}
+
+	public void setImageType(String imageType) {
+		this.imageType = imageType;
+	}
+
+	public byte[] getImageDate() {
+		return imageDate;
+	}
+
+	public void setImageDate(byte[] imageDate) {
+		this.imageDate = imageDate;
+	}
+    
+
+}
---
+++ Ecom-App/src/test/java/com/ecom/Ecom_App/EcomAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.Ecom_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
